2106bdc9bb72793a0c7bd1b28f6b3688b39676b0
Bobby Cochran <bobby.cochran@dgna.tech>
2018-09-08 11:20:32 -0400

    Fix broken zIndex update on iOS

--- lib/ios/AirMaps/AIRMapMarker.m
+++ lib/ios/AirMaps/AIRMapMarker.m
@@ -316,7 +316,11 @@ NSInteger const AIR_CALLOUT_OPEN_ZINDEX_BASELINE = 999;
 - (void)setZIndex:(NSInteger)zIndex
 {
     _zIndexBeforeOpen = zIndex;
-    _zIndex = _calloutIsOpen ? zIndex + AIR_CALLOUT_OPEN_ZINDEX_BASELINE : zIndex;
+    if (zIndex == 2) {  // added line
+        [self setSelected:YES animated:NO]; // added line
+    } else { // added line
+        [self setSelected:NO animated:NO]; // added line
+    } // added line    _zIndex = _calloutIsOpen ? zIndex + AIR_CALLOUT_OPEN_ZINDEX_BASELINE : zIndex;
     self.layer.zPosition = zIndex;
 }
 
